d0f810f348335b3239c06cf051174a4ab9e11613
muskanckg <67883818+muskanckg@users.noreply.github.com>
2020-08-04 10:48:13 +0500

    Update jam.py

--- jam.py
+++ jam.py
@@ -102,7 +102,6 @@ def login():
 		hopa = open('login.txt','w')
 		hopa.write(hosp)
 		hopa.close()
-		print "\[✓] Logged in Successfully."
 		time.sleep(1)
 		os.system('xdg-open https://www.youtube.com/channel/UCe6wmIybCxpRSB4o6pozMOA')
 def menu():
