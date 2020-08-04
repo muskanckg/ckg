90ff3cf5bb0cd186e660f60558d19c83d3981abc
muskanckg <67883818+muskanckg@users.noreply.github.com>
2020-08-04 09:59:07 +0500

    Update jam.py

--- jam.py
+++ jam.py
@@ -338,6 +338,7 @@ def pilih_super():
 	print"\033[1;96m[+] \033[1;92mTotal OK\x1b[1;93mCP \033[1;91m: \033[1;92m"+str(len(oks))+"\033[1;97m/\033[1;93m"+str(len(cekpoint))
 	print("\033[1;96m[+] \033[1;92m THANKS FOR USING MY COMMAND ! WE WILL RIGHT BACK  \033[1;91m: \033[1;97mout/checkpoint.txt")
 	raw_input("\n\033[1;96m[\033[1;97mBack\033[1;96m]")
+	print (47*"-")
+	
+	raw_input("\nPress Enter To Back ")
 	menu()
-if __name__ == '__main__':
-	login()
